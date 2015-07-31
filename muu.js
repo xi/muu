fd3938eb3ebf943275e7fb9f12f2fb731884c925
Tobias Bengfort <tobias.bengfort@gmx.net>
2015-07-31 13:44:59 +0200

    update example

--- muu.js
+++ muu.js
@@ -10,7 +10,7 @@ require(['xhr', 'registry', 'jqlite'], function(xhr, Registry, $) {
     "use strict";
 
     var registry = new Registry();
-    var template = '<ul>{{#elements}}<li>{{name}}</li>{{/elements}}</ul>' +
+    var template = '<ul>{{#elements}}<li><muu type="calc" data-name="{{name}}"></muu></li>{{/elements}}</ul>' +
         '<input name="input" data-onkeydown="push" />';
 
     registry.registerDirective('test', template, function(self) {
@@ -27,6 +27,24 @@ require(['xhr', 'registry', 'jqlite'], function(xhr, Registry, $) {
                 });
                 self.update(data);
                 self.setModel('input', '');
+            }
+        });
+
+        $.ready(function() {
+            self.update(data);
+        });
+    });
+
+    registry.registerDirective('calc', '<input name="input" data-onkeydown="update"/><span>{{result}}</span>', function(self, element) {
+        var data = {
+            name: element.getAttribute('data-name')
+        };
+
+        self.on('update', function(event) {
+            if (event.keyCode === 13) {
+                var input = self.getModel('input');
+                data.result = eval(input);
+                self.update(data);
             }
         });
 
