5f1d95c29c5af6ef87865691cdde7ceae0e60cda
Tobias Bengfort <tobias.bengfort@gmx.net>
2015-07-31 16:01:11 +0200

    add phonecat example
    from angular tutorial

---
+++ phonecat/phonecat.js
@@ -0,0 +1,44 @@
+requirejs.config({
+    baseUrl: '..',
+    paths: {
+        mustache: '../bower_components/mustache/mustache',
+        xhr: '../bower_components/promise-xhr/promise-xhr'
+    }
+});
+
+require(['xhr', 'muu', 'dom-helpers'], function(xhr, Muu, $) {
+    "use strict";
+
+    Promise.all([
+        xhr.get('phonecat.html'),
+        xhr.getJSON('phones.json')
+    ]).then(function(args) {
+        var template = args[0];
+        var phones = args[1];
+
+        new Muu()
+            .registerDirective('phonecat', template, function(self) {
+                self.on('filter', function() {
+                    var query = self.getModel('query', '').toLowerCase();
+                    var orderProp = self.getModel('orderProp');
+
+                    self.update({
+                        phones: phones
+                            .filter(function(phone) {
+                                return phone.name.toLowerCase().match(query);
+                            })
+                            .sort(function(a, b) {
+                                return a[orderProp] > b[orderProp];
+                            })
+                    });
+                });
+
+                $.ready(function() {
+                    self.update({phones: phones});
+                    self.setModel('query', '');
+                    self.setModel('orderProp', 'age');
+                });
+            })
+            .linkAll(document);
+    });
+});
