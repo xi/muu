57a666d485a92bd74786134253a48df12d3fc90e
Tobias Bengfort <tobias.bengfort@gmx.net>
2015-08-06 00:46:13 +0200

    create template tests

---
+++ test/test-template.js
@@ -0,0 +1,68 @@
+/* global define, describe, it, expect */
+define(['muu-template'], function(muuTemplate) {
+    "use strict";
+
+    describe('muuTemplate', function() {
+        it('does variable replacement', function() {
+            var template = '{{asd}}';
+            var result = muuTemplate(template, {asd: 'huhu'});
+            expect(result).to.equal('huhu');
+        });
+        it('does loops', function() {
+            var template = '{{#loop}}test{{/loop}}';
+            var result = muuTemplate(template, {loop: [1,2,3]});
+            expect(result).to.equal('testtesttest');
+        });
+        it('does loops with content', function() {
+            var template = '{{#loop}}{{name}} {{/loop}}';
+            var result = muuTemplate(template, {loop: [{name: 'tony'}, {name: 'barbara'}]});
+            expect(result).to.equal('tony barbara ');
+        });
+        it('does if', function() {
+            var template = '{{#truthy}}test1{{/truthy}} {{#falsy}}test2{{/falsy}}';
+            var result = muuTemplate(template, {truthy: true, falsy: false});
+            expect(result).to.equal('test1 ');
+        });
+        it('does pairs', function() {
+            var template = '{{str1: key1, str2: key2, str3: key3}}';
+            var result = muuTemplate(template, {key1: true, key2: false, key3: true});
+            expect(result).to.equal('str1 str3');
+        });
+        it('does comments', function() {
+            var template = 'foo {{!comment}} bar';
+            var result = muuTemplate(template, {});
+            expect(result).to.equal('foo  bar');
+        });
+        it('escapes HTML in variables', function() {
+            var template = '{{asd}}';
+            var result = muuTemplate(template, {asd: '<"&>'});
+            expect(result).to.equal('&lt;&quot;&amp;&gt;');
+        });
+        it('escapes HTML in pairs', function() {
+            var template = '{{<"&>: key1, str2: key2, str3: key3}}';
+            var result = muuTemplate(template, {key1: true, key2: false, key3: true});
+            expect(result).to.equal('&lt;&quot;&amp;&gt; str3');
+        });
+        it('fails on unclosed loop', function() {
+            var template = '{{#loop}}test{{loop}}';
+            var fn = function() {
+                return muuTemplate(template, {loop: [1,2,3]});
+            };
+            expect(fn).to.throw('unclosed loop: loop');
+        });
+        it('fails on unexpected closing loop', function() {
+            var template = 'test{{/loop}}';
+            var fn = function() {
+                return muuTemplate(template, {loop: [1,2,3]});
+            };
+            expect(fn).to.throw('unexpected closing loop: {{/loop}}');
+        });
+        it('fails on unclosed tag', function() {
+            var template = '{{asd}';
+            var fn = function() {
+                return muuTemplate(template, {asd: 'huhu'});
+            };
+            expect(fn).to.throw('unclosed tag: {{asd}');
+        });
+    });
+});
--- test/test.js
+++
@@ -1,9 +0,0 @@
-define(['muu-template'], function(muuTemplate) {
-    describe('muuTemplate', function() {
-        it('do variable replacement', function() {
-            var template = '{{asd}}'
-            var result = muuTemplate(template, {asd: 'huhu'});
-            expect(result).to.equal('huhu');
-        });
-    });
-});
