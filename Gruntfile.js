cf740749bc087b0a53dae0f07e8d26b26888c74d
Tobias Bengfort <tobias.bengfort@gmx.net>
2015-08-22 18:30:16 +0200

    increase saucelabs timeout slightly

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,7 +96,7 @@ module.exports = function (grunt) {
                     testname: process.env.TRAVIS_REPO_SLUG + '/' + process.env.TRAVIS_JOB_NUMBER,
                     throttled: 3,
                     sauceConfig: {
-                        'max-duration': 20,
+                        'max-duration': 25,
                         'video-upload-on-pass': false
                     },
                     onProgress: reportProgress
